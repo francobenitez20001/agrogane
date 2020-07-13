fee881f1833c47584479e6ac59880e4d722437aa
Franco Benitez <fhbenitez@hcdn.gob.ar>
2020-07-13 17:49:04 -0300

    iconos en item servicios

--- src/components/ItemServicio/ItemServicio.js
+++ src/components/ItemServicio/ItemServicio.js
@@ -9,7 +9,7 @@ const ItemServicio = ({servicio}) => {
     return (
         <div>
             <div className="jumbotron item__servicio" onClick={()=>handleClickDescripcion(servicio.idServicio)}>
-                <i className="fab fa-facebook"></i>
+                <i className={servicio.icono}></i>
                 <h3 className="ml-4 mr-auto">{servicio.titulo}</h3>
                 <i className="fas fa-angle-down" name={servicio.titulo} onClick={()=>handleClickDescripcion(servicio.idServicio)}></i>
             </div>
